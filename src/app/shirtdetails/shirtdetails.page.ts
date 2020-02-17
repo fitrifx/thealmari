cf09dfc58f425ed2853f1663c07469e910585b9e
Amir Fitri <fi3oo96@gmail.com>
2020-02-17 09:51:13 +0800

    files update for some pages

--- src/app/shirtdetails/shirtdetails.page.ts
+++ src/app/shirtdetails/shirtdetails.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-shirtdetails',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shirtdetails.page.scss'],
 })
 export class ShirtdetailsPage implements OnInit {
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 300,
+    autoPlay : true,
+    pager : true
+  };
+
+  slidesDidLoad(slides: IonSlides) {
+    slides.startAutoplay();
+  }
 
   constructor() { }
 
